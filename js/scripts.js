24e4866e1be81ff6421b4e3164bcf89b6b6a60f3
BegoCaZa <begoxcz@hotmail.com>
2025-04-12 12:40:31 +0200

    atorada en el 4

--- js/scripts.js
+++ js/scripts.js
@@ -11,29 +11,10 @@ const buttonExample2Element = document.getElementById('button-example2');
 const example1Container = document.getElementById('example1');
 const example2Container = document.getElementById('example2');
 
-const supplies = [
-  'a medkit',
-  'a flashlight with half battery',
-  'some canned food',
-  'a rusty knife',
-  'a bottle of clean water',
-  'a pack of bullets',
-  'a piece of cloth (could be used as a bandage)',
-  'a lighter',
-  'an empty bottle (might be useful)',
-  'some herbs (smell weird)',
-  'a broken walkie-talkie',
-  'a protein bar, still edible',
-  'a map with strange markings'
-  ];
-  
-  const failureTexts = [
-  'Nothing but silence and dust. Abby returned empty-handed.',
-  'No supplies today. The world remains unforgiving.',
-  "She searched everywhere, but luck wasn't on her side."
-  ];
 
 //FUNCIONES
+
+//MODO SIMPLE
 
 //Ejemplo 1
 const sayHello = () => {
@@ -74,6 +55,100 @@ const changeRange = () => {
 }
 
 //EVENTOS
-// buttonExample1Element.addEventListener('click', sayHello);
+buttonExample1Element.addEventListener('click', sayHello);
 rangeInputElement.addEventListener('input', changeRange);
 buttonExample2Element.addEventListener('click', addHeader);
+
+//EJERCICIOS MODO PRO
+
+//EJERCICIO 3
+const supplies = [
+  'a medkit',
+  'a flashlight with half battery',
+  'some canned food',
+  'a rusty knife',
+  'a bottle of clean water',
+  'a pack of bullets',
+  'a piece of cloth (could be used as a bandage)',
+  'a lighter',
+  'an empty bottle (might be useful)',
+  'some herbs (smell weird)',
+  'a broken walkie-talkie',
+  'a protein bar, still edible',
+  'a map with strange markings'
+  ];
+  
+  const failureTexts = [
+  'Nothing but silence and dust. Abby returned empty-handed.',
+  'No supplies today. The world remains unforgiving.',
+  "She searched everywhere, but luck wasn't on her side."
+  ];
+
+
+//BOTONES
+
+const buttonExample3Element = document.getElementById('button-example3');
+
+//DIV
+const supplyHistoryElement = document.getElementById('supply-history');
+
+//VARIABLES GENERALES
+let dayCounter=0;//tiene que estar fuera de la funcion para que no se reinicie
+
+//FUNCIONES
+const searchSupply = () => {
+  dayCounter++; //incremento el contador de dias
+  const randomIndexSupply = Math.floor(Math.random() * supplies.length);
+  const randomSupply = supplies[randomIndexSupply]; //saco el supplie random
+  const randomIndexFailure = Math.floor(Math.random() * failureTexts.length);
+  const randomFailureText = failureTexts[randomIndexFailure]; //saco el failure text random
+  const listItem = document.createElement('li'); //creo el li
+
+  //50% posibilidad
+  if (Math.random() < 0.5) {
+    listItem.textContent = `Day ${dayCounter}: ${randomSupply}`;
+  } else {
+    listItem.textContent = `Day ${dayCounter}: ${randomFailureText}`;
+  }
+  
+  supplyHistoryElement.append(listItem);
+
+
+};
+
+//EVENTOS
+buttonExample3Element.addEventListener('click', searchSupply);
+
+//EJERCICIO 4
+
+const buttonExample4Element = document.getElementById('button-example4');
+const trafficZoneElement = document.getElementById('traffic-zone');
+
+const cars = ['🚗', '🚕', '🚙'];
+
+//Funciones
+
+const dodgeCar = () => {
+
+  const randomIndex = Math.floor(Math.random() * cars.length);
+  const randomCar = cars[randomIndex]; //coche random
+
+  const traffic = document.createElement('span');
+  traffic.textContent = randomCar;
+  trafficZoneElement.append(traffic);
+
+  const repetitionsAllowed = 3; //cantidad de coches que se pueden repetir
+
+  if (traffic.value === repetitionsAllowed) {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = `Bego, please be careful! This car model ${randomCar} is stalking you! 🚨`;
+    trafficZoneElement.append(errorMessage);
+  } ///no sirveeeee
+}
+
+//EVENTOS
+buttonExample4Element.addEventListener('click', dodgeCar);
+
+
+
+
